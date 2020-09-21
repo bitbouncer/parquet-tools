644b1d8f7bd97fc36137cc77301c8c39531cf5eb
svante karlsson <svante.karlsson@csi.se>
2020-09-21 15:24:07 +0200

    optimize build scripts

--- 3rdparty_install.sh
+++ 3rdparty_install.sh
@@ -12,6 +12,7 @@ export DOUBLE_CONVERSION_VER="v3.1.5"
 export BROTLI_VER="v1.0.7"
 export FLATBUFFERS_VER="v1.11.0"
 export THRIFT_VER="0.12.0"
+export RAPIDJSON_VER="v1.1.0"
 
 export ARROW_VER="apache-arrow-1.0.1"
 
@@ -22,7 +23,7 @@ rm -rf tmp
 mkdir tmp
 cd tmp
 
-
+export BOOST_B2_LIBS="--with-program_options --with-thread --with-iostreams --with-filesystem --with-regex --with-system --with-date_time --with-chrono --with-test"
 wget -O boost.tar.gz "https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.tar.gz" && \
 mkdir -p boost && \
 tar \
@@ -32,11 +33,18 @@ tar \
   --strip-components 1
 cd boost
 ./bootstrap.sh
-./b2 cxxstd=17 --with-program_options --with-iostreams --with-filesystem --with-regex --with-system --with-date_time  -j "$(getconf _NPROCESSORS_ONLN)" stage
-sudo ./b2 cxxstd=17 --with-program_options --with-iostreams --with-filesystem --with-regex --with-system --with-date_time install
+./b2 cxxstd=$CPP_STANDARD  $BOOST_B2_LIBS -j "$(getconf _NPROCESSORS_ONLN)" stage
+sudo ./b2 cxxstd=$CPP_STANDARD $BOOST_B2_LIBS install
 cd ..
-rm boost.tar.gz
-rm -rf boost
+
+git clone --recursiv --depth 1 --branch $GRPC_VER https://github.com/grpc/grpc.git
+cd grpc
+mkdir build && cd build
+cmake ..
+make -j "$(getconf _NPROCESSORS_ONLN)" && \
+sudo make install && \
+cd ../..
+
 
 wget -O avro.tar.gz "https://github.com/apache/avro/archive/$AVRO_VER.tar.gz"
 mkdir -p avro
@@ -47,50 +55,31 @@ tar \
   --strip-components 1
 sed -i.bak1 's/-std=c++11/-std=c++17/g' avro/lang/c++/CMakeLists.txt
 sed -i.bak2 '/regex system)/a SET(Boost_LIBRARIES boost_program_options boost_iostreams boost_filesystem boost_regex boost_system z bz2)' avro/lang/c++/CMakeLists.txt
-#sed -i.bak3 '/find_package (Boost/d' avro/lang/c++/CMakeLists.txt
-#sed -i.bak4 '/regex system)/d' avro/lang/c++/CMakeLists.txt
 cat avro/lang/c++/CMakeLists.txt
 cd avro/lang/c++/ 
-mkdir build 
+mkdir -p build 
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release .. -DBUILD_SHARED_LIBS=ON -DCMAKE_CXX_STANDARD=$CPP_STANDARD
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_CXX_STANDARD=$CPP_STANDARD ..
 make -j "$(getconf _NPROCESSORS_ONLN)"
 sudo make install
 cd ../../../..
-rm avro.tar.gz
-rm -rf arvo
 
-git clone --recursiv --depth 1 --branch $GRPC_VER https://github.com/grpc/grpc.git
-cd grpc
-mkdir build && cd build
-cmake ..
-make -j "$(getconf _NPROCESSORS_ONLN)" && \
-sudo make install && \
-cd ../..
 
-git clone https://github.com/awslabs/aws-c-common
-cd aws-c-common
-mkdir build && cd build
-cmake .. 
-make -j "$(getconf _NPROCESSORS_ONLN)" 
-sudo make install 
-cd ../..
+#git clone https://github.com/awslabs/aws-checksums
+#cd aws-checksums
+#mkdir build && cd build
+#cmake -DCMAKE_PREFIX_PATH=/usr/local -DCMAKE_INSTALL_PREFIX=/usr/local ..
+#make -j "$(getconf _NPROCESSORS_ONLN)" 
+#sudo make install
+#cd ../..
 
-git clone https://github.com/awslabs/aws-checksums
-cd aws-checksums
-mkdir build && cd build
-cmake -DCMAKE_PREFIX_PATH=/usr/local -DCMAKE_INSTALL_PREFIX=/usr/local ..
-make -j "$(getconf _NPROCESSORS_ONLN)" 
-sudo make install
-cd ../..
-
-git clone https://github.com/awslabs/aws-c-event-stream
-cd aws-c-event-stream
-mkdir build && cd build
-cmake -DCMAKE_PREFIX_PATH=/usr/local -DCMAKE_INSTALL_PREFIX=/usr/local ..
-make -j "$(getconf _NPROCESSORS_ONLN)" 
-sudo make install
-cd ../..
+#git clone https://github.com/awslabs/aws-c-event-stream
+#cd aws-c-event-stream
+#mkdir build && cd build
+#cmake -DCMAKE_PREFIX_PATH=/usr/local -DCMAKE_INSTALL_PREFIX=/usr/local ..
+#make -j "$(getconf _NPROCESSORS_ONLN)" 
+#sudo make install
+#cd ../..
 
 wget -O aws-sdk.tar.gz "https://github.com/aws/aws-sdk-cpp/archive/$AWS_SDK_VER.tar.gz" && \
 mkdir -p aws-sdk && \
@@ -100,13 +89,23 @@ tar \
   --directory aws-sdk \
   --strip-components 1
 cd aws-sdk
-mkdir build
-cd build
+
+mkdir build-shared
+cd build-shared
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DBUILD_ONLY="config;s3;transfer" -DENABLE_TESTING=OFF -DCPP_STANDARD=$CPP_STANDARD ..
-#cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DENABLE_TESTING=OFF -DCPP_STANDARD=$CPP_STANDARD ..
 make -j "$(getconf _NPROCESSORS_ONLN)"
 sudo make install
-cd ../..
+cd ..
+
+mkdir build-static
+cd build-static
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DBUILD_ONLY="config;s3;transfer" -DENABLE_TESTING=OFF -DCPP_STANDARD=$CPP_STANDARD ..
+make -j "$(getconf _NPROCESSORS_ONLN)"
+sudo make install
+cd ..
+
+cd ..
+
 
 wget -O double-conversion.tar.gz "https://github.com/google/double-conversion/archive/$DOUBLE_CONVERSION_VER.tar.gz" && \
 mkdir -p double-conversion && \
@@ -122,7 +121,6 @@ cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_CXX_STANDARD=$CP
 make -j "$(getconf _NPROCESSORS_ONLN)"
 sudo make install
 cd ../..
-rm double-conversion.tar.gz
 
 wget -O brotli.tar.gz "https://github.com/google/brotli/archive/$BROTLI_VER.tar.gz" && \
 mkdir -p brotli && \
@@ -138,7 +136,6 @@ cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_CXX_STANDARD=$CP
 make -j "$(getconf _NPROCESSORS_ONLN)"
 sudo make install
 cd ../..
-rm brotli.tar.gz
 
 
 wget -O flatbuffers.tar.gz "https://github.com/google/flatbuffers/archive/$FLATBUFFERS_VER.tar.gz" && \
@@ -155,7 +152,6 @@ cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DFLATBUFFERS_BUILD_TEST
 make -j "$(getconf _NPROCESSORS_ONLN)"
 sudo make install
 cd ../..
-rm flatbuffers.tar.gz
 
 wget -O thrift.tar.gz "https://github.com/apache/thrift/archive/$THRIFT_VER.tar.gz" && \
 mkdir -p thrift && \
@@ -166,11 +162,10 @@ tar \
   --strip-components 1 && \
 cd thrift && \
 mkdir -p build && cd build && \
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_CXX_STANDARD=$CPP_STANDARD .. && \
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_CXX_STANDARD=$CPP_STANDARD -DBUILD_JAVA=OFF -DBUILD_PYTHON=OFF -DBUILD_TESTING=OFF -DBUILD_TUTORIALS=OFF .. && \
 make -j "$(getconf _NPROCESSORS_ONLN)" && \
 sudo make install && \
-cd ../.. && \
-rm thrift.tar.gz
+cd ../..
 
 wget -O nlomann.tar.gz "https://github.com/nlohmann/json/archive/v$NLOHMANN_JSON_VER.tar.gz" && \
 mkdir -p nlomann && \
@@ -184,9 +179,22 @@ mkdir build && cd build
 cmake ..
 make -j "$(getconf _NPROCESSORS_ONLN)" && \
 sudo make install && \
-cd ../.. && \
-rm nlomann.tar.gz && \
-rm -rf nlomann
+cd ../..
+
+wget -O rapidjson.tar.gz "https://github.com/miloyip/rapidjson/archive/$RAPIDJSON_VER.tar.gz" && \
+mkdir -p rapidjson && \
+tar \
+   --extract \
+   --file rapidjson.tar.gz \
+   --directory rapidjson \
+   --strip-components 1 && \
+cd rapidjson && \
+mkdir build && \
+cd build && \
+cmake -DRAPIDJSON_BUILD_EXAMPLES=OFF -DRAPIDJSON_BUILD_DOC=OFF -DRAPIDJSON_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=ON -DCMAKE_CXX_STANDARD=$CPP_STANDARD .. && \
+sudo make install && \
+sudo rm -rf /usr/local/share/doc/RapidJSON && \
+cd ../..
 
 
 wget -O arrow.tar.gz "https://github.com/apache/arrow/archive/$ARROW_VER.tar.gz" && \
@@ -230,7 +238,6 @@ cmake \
 make -j "$(getconf _NPROCESSORS_ONLN)"
 sudo make install
 cd ../..
-rm arrow.tar.gz
 
 #out of tmp
 cd ..
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # parquet-tools
+sudo apt-get update
+sudo apt-get install -y sudo build-essential cmake wget pax-utils automake autogen shtool libtool unzip pkg-config sed bison flex git
+
+#boost deps
+sudo apt-get install -y libz-dev liblzma-dev libzstd-dev
+
+#avro deps
+sudo apt-get install -y libsnappy-dev
+
+#aws deps
+sudo apt-get install -y libbz2-dev libssl-dev libcurl4-openssl-dev libgoogle-glog-dev
+
+#deps for arrow
+sudo apt-get install -y liblz4-dev libutf8proc-dev libre2-dev llvm-7 clang
+
